9edf5b139067bb62949a403c86bc479612571c88
Diego Freile Garcia <dfreile51@gmail.com>
2023-10-17 17:19:17 +0200

    Funcionalidad de Crear Cuenta realizada

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -2,6 +2,8 @@
 
 namespace Controllers;
 
+use Classes\Email;
+use Model\Usuario;
 use MVC\Router;
 
 class LoginController
@@ -9,7 +11,6 @@ class LoginController
     public static function login(Router $router)
     {
         if ($_SERVER["REQUEST_METHOD"] === "POST") {
-            
         }
 
         // Render a la vista
@@ -25,20 +26,54 @@ class LoginController
 
     public static function crear(Router $router)
     {
+        $usuario = new Usuario();
+        $alertas = [];
+
         if ($_SERVER["REQUEST_METHOD"] === "POST") {
-            
+            $usuario->sincronizar($_POST); // LLenar el objeto usuario con los datos del formulario
+            $alertas = $usuario->validarNuevaCuenta(); // Verificar campos del formulario
+
+            if (empty($alertas)) {
+                $existeUsuario = Usuario::where("email", $usuario->email);
+
+                if ($existeUsuario) {
+                    Usuario::setAlerta("error", "El usuario ya esta registrado");
+                    $alertas = Usuario::getAlertas();
+                } else {
+                    // Hashear el password
+                    $usuario->hashPassword();
+
+                    // Eliminar password2 del objeto
+                    unset($usuario->password2);
+
+                    // Generar el token
+                    $usuario->crearToken();
+
+                    // Crear un nuevo usuario
+                    $resultado = $usuario->guardar();
+
+                    // Enviar Email
+                    $email = new Email($usuario->email, $usuario->nombre, $usuario->token);
+                    $email->enviarConfirmacion();
+
+                    if ($resultado) {
+                        header("Location: /mensaje");
+                    }
+                }
+            }
         }
 
         // Render a la vista
         $router->render("auth/crear", [
-            "titulo" => "Crea tu cuenta en UpTask"
+            "titulo" => "Crea tu cuenta en UpTask",
+            "usuario" => $usuario,
+            "alertas" => $alertas
         ]);
     }
 
     public static function olvide(Router $router)
     {
         if ($_SERVER["REQUEST_METHOD"] === "POST") {
-            
         }
 
         // Render a la vista
@@ -50,7 +85,6 @@ class LoginController
     public static function reestablecer(Router $router)
     {
         if ($_SERVER["REQUEST_METHOD"] === "POST") {
-            
         }
 
         // Render a la vista
@@ -68,8 +102,33 @@ class LoginController
 
     public static function confirmar(Router $router)
     {
+        $token = s($_GET["token"]);
+
+        if (!$token) header("Location: /");
+
+        // Encontrar al usuario con este token
+        $usuario = Usuario::where("token", $token);
+
+        if (empty($usuario)) {
+            // No se encontró un usuario con ese token
+            Usuario::setAlerta("error", "Token No Válido");
+        } else {
+            // Confirmar la cuenta
+            $usuario->confirmado = 1;
+            $usuario->token = null;
+            unset($usuario->password2);
+            
+            // Guardar en la base de datos
+            $usuario->guardar();
+
+            Usuario::setAlerta("exito", "Cuenta Comprobada Correctamente");
+        }
+
+        $alertas = Usuario::getAlertas();
+
         $router->render("auth/confirmar", [
-            "titulo" => "Confirma tu cuenta UpTask"
+            "titulo" => "Confirma tu cuenta UpTask",
+            "alertas" => $alertas
         ]);
     }
 }
--- views/auth/crear.php
+++ views/auth/crear.php
@@ -3,15 +3,16 @@
 
     <div class="contenedor-sm">
         <p class="descripcion-pagina">Crea tu cuenta en UpTask</p>
+        <?php include_once __DIR__ . "/../templates/alertas.php"; ?>
 
         <form class="formulario" method="POST" action="/crear">
             <div class="campo">
                 <label for="nombre">Nombre</label>
-                <input type="text" name="nombre" id="nombre" placeholder="Tu Nombre">
+                <input type="text" name="nombre" id="nombre" placeholder="Tu Nombre" value="<?php echo $usuario->nombre; ?>">
             </div>
             <div class="campo">
                 <label for="email">Email</label>
-                <input type="email" name="email" id="email" placeholder="Tu Email">
+                <input type="email" name="email" id="email" placeholder="Tu Email" value="<?php echo $usuario->email; ?>">
             </div>
             <div class="campo">
                 <label for="password">Password</label>
