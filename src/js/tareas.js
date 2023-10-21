2d1b67744982935204f1e50aeb8ecb81fcc5d294
Diego Freile Garcia <dfreile51@gmail.com>
2023-10-21 17:08:34 +0200

    Ventana modal para añadir tarea realizada correctamente

---
+++ src/js/tareas.js
@@ -0,0 +1,45 @@
+(function () {
+  // Botón para mostrar el Modal de Agregar tarea
+  const nuevaTareaBtn = document.querySelector("#agregar-tarea");
+
+  nuevaTareaBtn.addEventListener("click", mostrarFormulario);
+
+  function mostrarFormulario() {
+    const modal = document.createElement("DIV");
+
+    modal.classList.add("modal");
+    modal.innerHTML = `
+            <form class="formulario nueva-tarea">
+                <legend>Añade una nueva tarea</legend>
+                <div class="campo">
+                    <label for="tarea">Tarea</label>
+                    <input type="text" name="tarea" placeholder="Añadir Tarea al Proyecto Actual" id="tarea">
+                </div>
+                <div class="opciones">
+                    <input type="submit" class="submit-nueva-tarea" value="Añadir Tarea">
+                    <button type="buton" class="cerrar-modal">Cancelar</button>
+                </div>
+            </form>
+        `;
+
+    setTimeout(() => {
+      const formulario = document.querySelector(".formulario");
+      formulario.classList.add("animar");
+    }, 0);
+
+    modal.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      if (e.target.classList.contains("cerrar-modal")) {
+        const formulario = document.querySelector(".formulario");
+        formulario.classList.add("cerrar");
+
+        setTimeout(() => {
+          modal.remove();
+        }, 500);
+      }
+    });
+
+    document.querySelector("body").appendChild(modal);
+  }
+})();
